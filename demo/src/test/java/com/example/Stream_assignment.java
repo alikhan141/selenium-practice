2c8a8bbcdbbf20b807b38b80fd145a8336975343
alikhan141 <alikhantd2025@gmail.com>
2025-07-23 12:02:27 +0500

    new file

--- demo/src/test/java/com/example/Stream_assignment.java
+++ demo/src/test/java/com/example/Stream_assignment.java
@@ -1,11 +1,15 @@
 package com.example;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.Assert;
 
 public class Stream_assignment {
     public static void main(String[] args) {
@@ -19,6 +23,9 @@ public class Stream_assignment {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
         driver.findElement(By.cssSelector("th[role*='columnheader']:nth-child(1)")).click();
+        List<WebElement> elements = driver.findElements(By.xpath("//tr/td[1]"));
+        elements.stream().map(s -> s.getText()).sorted().collect(Collectors.toList());
+
         driver.quit();
     }
 }
